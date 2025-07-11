8dd0d6c7b7c7c32aa3f89000af041ddd93c366d1
OUIFKI-AYOUB <150385854+OUIFKI-AYOUB@users.noreply.github.com>
2025-07-11 18:29:56 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your other Next.js config options
+    eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
