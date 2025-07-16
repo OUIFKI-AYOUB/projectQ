1f8ef7fdb95a59f5eae3ea5cf7cb5b8a526e6681
OUIFKI AYOUB <ouifki400@gmail.com>
2025-07-16 21:44:10 +0100

    your message here

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
     ignoreDuringBuilds: true, // Ignore ESLint errors during build
   },
   // Your other Next.js configuration options...
+    webpack: (config) => {
+    config.externals.push('@prisma/client');
+    return config;
+  }
 };
 
 module.exports = nextConfig;
